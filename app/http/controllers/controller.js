e64d7f8543735e1f283a85e1dfcfa2c815872866
salararjmandpour <salararjmand4@gmail.com>
2023-07-06 17:59:51 +0330

    new push

--- app/http/controllers/controller.js
+++ app/http/controllers/controller.js
@@ -1,29 +1,24 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
 const autoBind = require("auto-bind");
-
 
 //>---------------------- module export
 module.exports = class controller {
-    constructor() {
-        autoBind(this);
-        this.createDate();
+  constructor() {
+    autoBind(this);
+    this.createDate();
+  }
 
-    }
+  //>---------------------- create Date of os
+  createDate() {
+    const date = new Date();
+    const option = {
+      weekday: "long",
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    };
 
-    //>---------------------- create Date
-    createDate(){
-
-        const date = new Date();
-        const option = {
-          weekday: "long",
-          year: "numeric",
-          month: "numeric",
-          day: "numeric",
-        };
-
-        let totalDate = date.toLocaleDateString("fa-IR", option);
-        return totalDate;
-    }
-}
-
-
+    let totalDate = date.toLocaleDateString("fa-IR", option);
+    return totalDate;
+  }
+};
