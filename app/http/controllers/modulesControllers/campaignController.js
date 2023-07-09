a04c58d47fd663f752aa477ef25b530c9bd8dc47
salararjmandpour <salararjmand4@gmail.com>
2023-07-09 10:33:24 +0330

    create controller campaign

---
+++ app/http/controllers/modulesControllers/campaignController.js
@@ -0,0 +1,95 @@
+const User = require("app/models/User");
+const Clues = require("app/models/Clue");
+const CampaignMain = require("app/models/CampaignMain");
+
+//>---------- encrypt data sending
+
+const cerateCipher = require("../../middleware/cerateCipher");
+const Key = config.encryptionKey;
+
+//>----------- post route campaign
+
+const createHandler = async (req, res) => {
+  const dataDecrypt = await JSON.parse(
+    cerateCipher.decrypt(req.body.dataEnc, Key)
+  );
+  const expertDecrypt = await cerateCipher.decrypt(req.body.getId.userId, Key);
+  let expertFullName = await User.findOne({ _id: expertDecrypt });
+  expertFullName = expertFullName.fullName;
+
+  const {
+    campaignName,
+    campaignStatus,
+    campaignTimeStart,
+    campaignTimeEnd,
+    campaignKind,
+    campaignNumberOfContacts,
+    campaignResponse,
+    campaignMoney,
+    campaignIncome,
+    campaignCost,
+    campaignGoal,
+    campaignNote,
+  } = dataDecrypt;
+
+  if (
+    !campaignName ||
+    !campaignStatus ||
+    !campaignTimeStart ||
+    !campaignTimeEnd ||
+    !campaignKind ||
+    !campaignNumberOfContacts ||
+    !campaignResponse ||
+    !expertFullName ||
+    !campaignMoney ||
+    !campaignIncome ||
+    !campaignCost ||
+    !campaignGoal ||
+    !campaignNote
+  ) {
+    return res.status(400);
+  }
+
+  try {
+
+    //>----------- create model for data campaign 
+
+    await CampaignMain.create({
+      campaignName: campaignName,
+      campaignStatus: campaignStatus,
+      campaignTimeStart: campaignTimeStart,
+      campaignTimeEnd: campaignTimeEnd,
+      campaignKind: campaignKind,
+      campaignNumberOfContacts: campaignNumberOfContacts,
+      campaignResponse: campaignResponse,
+      campaignMoney: campaignMoney,
+      campaignIncome: campaignIncome,
+      campaignCost: campaignCost,
+      campaignGoal: campaignGoal,
+      campaignNote: campaignNote,
+      userId: expertDecrypt,
+    });
+
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ message: err });
+  }
+};
+
+//>---------- get data campaign all 
+
+const getHandler = async (req, res) => {
+  const campaignMain = await CampaignMain.find({});
+
+  if (!campaignMain || campaignMain.length == 0)
+    return res.status(304).send("هیچ کمپینی وجود ندارد");
+
+  const encryptData = cerateCipher.encrypt(JSON.stringify(campaignMain), Key);
+  return res.status(202).json({
+    encryptData,
+  });
+};
+//>------------ export method
+
+module.exports = { createHandler, getHandler };
