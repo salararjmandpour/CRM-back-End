8f5aecac0b13914e664a28e6ccd2df09ce2ffd04
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-07-16 13:47:02 +0330

    route phoneCode]

---
+++ app/routers/api/v1/phoneCode.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+//>---------- import tools
+
+const getPhoneCode = require("app/http/tools/states");
+
+//>---------- create routers
+
+router.get("/", getPhoneCode.getPhoneCode);
+
+module.exports = router;
