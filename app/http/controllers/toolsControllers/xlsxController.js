f42a8270b6af80cfe34e4f5755e251f53bf092bd
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-07-19 15:41:06 +0330

    create export method

--- app/http/controllers/toolsControllers/xlsxController.js
+++ app/http/controllers/toolsControllers/xlsxController.js
@@ -5,87 +5,202 @@ const mkdirp = require("mkdirp");
 const fs = require("fs");
 const xlsx = require("xlsx");
 const User = require("app/models/User");
-
-
-
+const Clues = require("app/models/Clue");
 
 //>---------- encrypt data sending
 
 const cerateCipher = require("../../middleware/cerateCipher");
 const Key = config.encryptionKey;
 
+let fullNameFile = "";
 
+//>---------- create Date
+const createDate = () => {
+  const date = new Date();
+  const option = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  };
 
+  let totalDate = date.toLocaleDateString("fa-IR", option);
+  totalDate = totalDate.replaceAll("/", "-");
+  return totalDate;
+};
 
-let fullName = "";
+//>---------- create method import xlsx of clues
 
-const uploadFile = async (req, res, next) => {
+const importFile = async (req, res, next) => {
+  if (req.query.userid) {
+    const strId = req.query.userid.toString();
+    const strIdNew = strId.replaceAll(" ", "+");
+    console.log(strIdNew);
+    const expertDecrypt = await cerateCipher.decrypt(strIdNew, Key);
+    let expertFullName = await User.findOne({ _id: expertDecrypt });
+    expertFullName = expertFullName.fullName;
+
+    const storage = multer.diskStorage({
+      destination: (req, file, callback) => {
+        console.log(file);
+        callback(null, "./upload");
+      },
+      filename: (req, file, callback) => {
+        console.log(file.originalname);
+        const temp_file_arr = file.originalname.split(".");
+
+        const temp_file_name = temp_file_arr[0];
+
+        const temp_file_extension = temp_file_arr[1];
+
+        callback(
+          null,
+          temp_file_name + "-" + Date.now() + "." + temp_file_extension
+        );
+      },
+    });
+
+    const upload = multer({ storage: storage }).single("file");
+
+    upload(req, res, async (error) => {
+      if (error) {
+        console.log(error);
+        return res.end("Error Uploading File");
+      } else {
+        fullNameFile = req.file.path;
+        // console.log("success", req.file.filename);
+      }
+
+      const wb = xlsx.readFile(fullNameFile, {
+        cellDates: true,
+      });
+      const ws = wb.Sheets["Sheet1"];
+
+      const data = xlsx.utils.sheet_to_json(ws);
+
+      try {
+        for (let index = 0; index < data.length; index++) {
+          console.log(`index : ${index}`, data[index]);
+
+          const {
+            subject,
+            fullName,
+            role,
+            mobile,
+            industry,
+            company,
+            phonNumber,
+            state,
+            cities,
+            address,
+            qualityCustomer,
+            callTime,
+          } = data[index];
+
+          if (
+            !subject ||
+            !fullName ||
+            !role ||
+            !mobile ||
+            !industry ||
+            !expertDecrypt ||
+            !expertFullName ||
+            !company ||
+            !phonNumber ||
+            !state ||
+            !cities ||
+            !address ||
+            !callTime ||
+            !qualityCustomer
+          ) {
+            return res
+              .status(400)
+              .json({ message: "فایل مورد نظر دارای یک فیلد خالی می باشد " });
+          }
+
+          const duplicate = await Clues.findOne({ mobile, phonNumber }).exec();
+
+          if (duplicate) return res.status(409);
+
+          console.log("subject :", subject);
+          await Clues.create({
+            subject: subject,
+            fullName: fullName,
+            role: role,
+            mobile: mobile,
+            industry: industry,
+            expert: expertDecrypt,
+            expertFullName: expertFullName,
+            company: company,
+            phonNumber: phonNumber,
+            state: state,
+            cities: cities,
+            address: address,
+            qualityCustomer: qualityCustomer,
+            callTime: callTime,
+          });
+        }
+
+        fs.unlinkSync(fullNameFile);
+
+        return res.sendStatus(201);
+      } catch (err) {
+        console.log(err.message);
+        return res.status(500).json({ message: err });
+      }
+    });
+  } else {
+    return res.status(409);
+  }
+};
+
+//>---------- create method export xlsx of clues
+
+const exportFile = async (req, res, next) => {
   const strId = req.query.userid.toString();
   const strIdNew = strId.replaceAll(" ", "+");
-  console.log(strIdNew);
+
   const expertDecrypt = await cerateCipher.decrypt(strIdNew, Key);
-  let expertFullName = await User.findOne({ _id: expertDecrypt });
-  expertFullName = expertFullName.fullName;
-  console.log(`id: ${expertDecrypt} userName: ${expertFullName}`);
 
-  const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-      console.log(file);
-      callback(null, "./upload");
+  const exportFile = await Clues.find(
+    {
+      expert: expertDecrypt,
     },
-    filename: (req, file, callback) => {
-      console.log(file.originalname);
-      const temp_file_arr = file.originalname.split(".");
-
-      const temp_file_name = temp_file_arr[0];
-
-      const temp_file_extension = temp_file_arr[1];
-
-      callback(
-        null,
-        temp_file_name + "-" + Date.now() + "." + temp_file_extension
-      );
-    },
-  });
-
-  const upload = multer({ storage: storage }).single("file");
-
-  upload(req, res, (error) => {
-    if (error) {
-      console.log(error);
-      return res.end("Error Uploading File");
-    } else {
-      fullName = req.file.path;
-      console.log("success", req.file.filename);
-
-      // response.redirect("/fileupload");
+    {
+      _id: 0,
+      expert: 0,
+      campaign: 0,
+      createdAt: 0,
+      updatedAt: 0,
+      __v: 0,
     }
-    // const file = `../../../../upload/${fullName}`;
+  );
 
-    console.log(fullName);
-    const wb = xlsx.readFile(fullName, {
-      cellDates: true,
-    });
-    const ws = wb.Sheets["Sheet1"];
+  let newData = JSON.parse(JSON.stringify(exportFile));
 
-    const data = xlsx.utils.sheet_to_json(ws);
-
-    console.log(data);
-
-    fs.unlinkSync(fullName);
-
-    // res.status(200).end(xlsx.utils.sheet_to_csv(ws));
-
-    return res.end("File is uploaded successfully");
+  newData.map((record) => {
+    delete record.id;
+    return record;
   });
+
+  const ws = xlsx.utils.json_to_sheet(newData);
+  const wb = xlsx.utils.book_new();
+  xlsx.utils.book_append_sheet(wb, ws, "Sheet1");
+  let filename = `./upload/${createDate()} Export Clues.xlsx`;
+  xlsx.writeFile(wb, filename);
+
+  res.download(filename);
+
+  setTimeout(() => {
+    fs.unlinkSync(filename);
+  }, "2000");
 };
+
+//>---------- create method example xlsx of clues
 
 const downloadFile = (req, res, next) => {
   res.download("./upload/Example Data.zip");
-
-  next();
 };
 
 //>---------- module exports
 
-module.exports = { uploadFile, downloadFile };
+module.exports = { importFile, exportFile, downloadFile };
