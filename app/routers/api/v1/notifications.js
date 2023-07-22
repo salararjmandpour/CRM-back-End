ced369997f71d6650191a38190a63c0f8fea7e21
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-07-22 14:12:38 +0330

    create route notifications

---
+++ app/routers/api/v1/notifications.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+//>----------- import controllers
+
+const notificationsController = require("app/http/controllers/modulesControllers/notificationsControllers");
+
+//>---------- import middleware
+
+const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
+
+//>---------- create route
+
+router.get("/get", verifyJWT, notificationsController.getNotifications);
+
+module.exports = router;
