8315e234dfc2a1bdd64defd8a9806a79258c1238
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-07-31 12:06:38 +0330

    edit comment

--- app/routers/api/v1/contacts.js
+++ app/routers/api/v1/contacts.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-//>---------- controller
+//*>---------- controller
 
 const ContactController = require("app/http/controllers/modulesControllers/contactsControllers");
 
-//>---------- middleware
+//*>---------- middleware
 
 const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
 
-//>----------- create route for clue
+//*>----------- create route for clue
 
 router.post("/", verifyJWT, ContactController.createHandler);
 
