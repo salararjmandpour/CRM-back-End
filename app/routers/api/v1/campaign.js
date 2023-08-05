1d9c1d53368d0df2f5821c8576a54f762e7304ed
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-05 17:42:27 +0330

    create middleware

--- app/routers/api/v1/campaign.js
+++ app/routers/api/v1/campaign.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const ROLES_LIST = require("../../../config/roles_list");
 
 //>---------- controller
 
@@ -8,16 +9,41 @@ const campaignController = require("app/http/controllers/modulesControllers/camp
 //>---------- middleware
 
 const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
+const verifyRoles = require("app/http/middleware/authMiddleware/verifyRoles");
 
 //>----------- create route for campaign
 
-router.get("/", verifyJWT, campaignController.getSingleAndAllHandler);
+router.get(
+  "/",
+  verifyJWT,
+  verifyRoles(
+    ROLES_LIST.SeniorManager,
+    ROLES_LIST.SalesManager,
+    ROLES_LIST.User
+  ),
+  campaignController.getSingleAndAllHandler
+);
 
-router.post("/", verifyJWT, campaignController.createHandler);
+router.post(
+  "/",
+  verifyJWT,
+  verifyRoles(ROLES_LIST.SeniorManager, ROLES_LIST.SalesManager),
+  campaignController.createHandler
+);
 
-router.delete("/", verifyJWT, campaignController.deleteOneCampaign);
+router.delete(
+  "/",
+  verifyJWT,
+  verifyRoles(ROLES_LIST.SeniorManager, ROLES_LIST.SalesManager),
+  campaignController.deleteOneCampaign
+);
 
-router.put("/", verifyJWT, campaignController.updateOneCampaign);
+router.put(
+  "/",
+  verifyJWT,
+  verifyRoles(ROLES_LIST.SeniorManager, ROLES_LIST.SalesManager),
+  campaignController.updateOneCampaign
+);
 
 //>----------- exports routers
 
