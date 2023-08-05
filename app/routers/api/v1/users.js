5e647f531f382780c8af06ed06772dbcf84114c3
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-05 17:43:10 +0330

    edit code

--- app/routers/api/v1/users.js
+++ app/routers/api/v1/users.js
@@ -9,9 +9,15 @@ const userController = require("app/http/controllers/usersControllers/usersContr
 
 const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
 const verifyRoles = require("app/http/middleware/authMiddleware/verifyRoles");
+const ROLES_LIST = require("../../../config/roles_list");
 
 //>--------- routers
 
-router.post("/", verifyJWT, verifyRoles, userController.getAllUsers);
+router.post(
+  "/",
+  verifyJWT,
+  verifyRoles(ROLES_LIST.SeniorManager),
+  userController.getAllUsers
+);
 
 module.exports = router;
