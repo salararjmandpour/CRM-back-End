6bf6a2aa521adb1ee8274ee1a299fcbf42fd3d98
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-05 17:41:24 +0330

    edit code

--- app/http/middleware/authMiddleware/verifyRoles.js
+++ app/http/middleware/authMiddleware/verifyRoles.js
@@ -7,15 +7,28 @@ const ROLES_LIST = require("../../../config/roles_list");
 
 //>---------- verifyRoles
 
-const verifyRoles = (req, res) => {
-    const role = cerateCipher.decrypt(req.body.roles, Key);
-    if (!role) return res.sendStatus(401);
-    const result = ROLES_LIST.find(vary => vary.role === role);
-    if (!result) return res.sendStatus(401);
-    const encryptData = cerateCipher.encrypt(JSON.stringify(result), Key);
-    res.json({ encryptData });
+// const verifyRoles = (req, res) => {
+//     const role = cerateCipher.decrypt(req.query.roles, Key);;
+//     if (!role) return res.sendStatus(401);
+//     const roleInt = parseInt(role);
+//     const result = ROLES_LIST.find((vary) => vary.role === roleInt);
+//     if (!result) return res.sendStatus(401);
+//     const encryptData = cerateCipher.encrypt(JSON.stringify(result), Key);
+//     res.json({ encryptData });
     
+//   };
+
+const verifyRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req?.roles) return res.sendStatus(401);
+    const rolesArray = [...allowedRoles];
+    const result = req.roles
+      .map((role) => rolesArray.includes(role))
+      .find((val) => val === true);
+    if (!result) return res.sendStatus(401);
+    next();
   };
+};
 
 
 module.exports = verifyRoles;
