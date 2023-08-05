6eb6e6912e97659c9aad3c0c645140ab7612e9f0
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-05 17:42:38 +0330

    create middleware

--- app/routers/api/v1/clue.js
+++ app/routers/api/v1/clue.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const ROLES_LIST = require("../../../config/roles_list");
 
 //>---------- controller
 
@@ -8,6 +9,7 @@ const clueController = require("app/http/controllers/modulesControllers/clueCont
 //>---------- middleware
 
 const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
+const verifyRoles = require("app/http/middleware/authMiddleware/verifyRoles");
 
 
 //>----------- create route for clue
@@ -16,8 +18,26 @@ router.get("/singleandall", verifyJWT, clueController.getOneAndAllHandler);
 
 router.post("/", verifyJWT, clueController.createHandler);
 
-router.put("/", verifyJWT, clueController.updateOneClue);
+router.put(
+  "/",
+  verifyJWT,
+  verifyRoles(
+    ROLES_LIST.SeniorManager,
+    ROLES_LIST.SalesManager,
+    ROLES_LIST.SalesSpecialist,
+    ROLES_LIST.FinanceManager,
+    ROLES_LIST.AccountingManager,
+    ROLES_LIST.Accountant,
+    ROLES_LIST.CRMManager
+  ),
+  clueController.updateOneClue
+);
 
-router.delete("/", verifyJWT, clueController.deleteOneClue);
+router.delete(
+  "/",
+  verifyJWT,
+  verifyRoles(ROLES_LIST.SeniorManager, ROLES_LIST.SalesManager),
+  clueController.deleteOneClue
+);
 
 module.exports = router;
