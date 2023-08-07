345f40ca3b900e2e0957422e329d10bc3dd89530
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-07 17:47:18 +0330

    edit code

--- app/routers/api/v1/clue.js
+++ app/routers/api/v1/clue.js
@@ -11,12 +11,24 @@ const clueController = require("app/http/controllers/modulesControllers/clueCont
 const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
 const verifyRoles = require("app/http/middleware/authMiddleware/verifyRoles");
 
-
 //>----------- create route for clue
 
 router.get("/singleandall", verifyJWT, clueController.getOneAndAllHandler);
 
-router.post("/", verifyJWT, clueController.createHandler);
+router.post(
+  "/",
+  verifyJWT,
+  verifyRoles(
+    ROLES_LIST.SeniorManager,
+    ROLES_LIST.SalesSpecialist,
+    ROLES_LIST.SalesManager,
+    ROLES_LIST.FinanceManager,
+    ROLES_LIST.AccountingManager,
+    ROLES_LIST.Accountant,
+    ROLES_LIST.CRMManager,
+  ),
+  clueController.createHandler
+);
 
 router.put(
   "/",
