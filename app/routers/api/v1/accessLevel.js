6763a7cf4a242caff8790c2f6a64dee9f504a9ae
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-07 13:07:28 +0330

    create route access level

---
+++ app/routers/api/v1/accessLevel.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const ROLES_LIST = require("../../../config/roles_list");
+
+//*>---------- controller
+
+const accessLevelController = require("app/http/controllers/modulesControllers/accessLevelController");
+
+//*>---------- middleware
+
+const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
+const verifyRoles = require("app/http/middleware/authMiddleware/verifyRoles");
+
+//*>----------- create route for create side
+
+router.get(
+  "/",
+  verifyJWT,
+  verifyRoles(ROLES_LIST.HrManager, ROLES_LIST.SeniorManager),
+  accessLevelController.getUserRole,
+);
+
+router.put(
+  "/",
+  verifyJWT,
+  verifyRoles(ROLES_LIST.HrManager, ROLES_LIST.SeniorManager),
+  accessLevelController.putAccessLevelForUser,
+);
+module.exports = router;
