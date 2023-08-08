af4487367f880fc9bc43c3f395d66dea7bec3bed
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-08 17:54:28 +0330

    edit code

--- app/http/middleware/authMiddleware/verifyRoles.js
+++ app/http/middleware/authMiddleware/verifyRoles.js
@@ -7,17 +7,6 @@ const ROLES_LIST = require("../../../config/roles_list");
 
 //*>---------- verifyRoles
 
-// const verifyRoles = (req, res) => {
-//     const role = cerateCipher.decrypt(req.query.roles, Key);;
-//     if (!role) return res.sendStatus(401);
-//     const roleInt = parseInt(role);
-//     const result = ROLES_LIST.find((vary) => vary.role === roleInt);
-//     if (!result) return res.sendStatus(401);
-//     const encryptData = cerateCipher.encrypt(JSON.stringify(result), Key);
-//     res.json({ encryptData });
-    
-//   };
-
 const verifyRoles = (...allowedRoles) => {
   return (req, res, next) => {
     if (!req?.roles) return res.sendStatus(401);
