ed8922ebab072152b8a088daa2954a21b05751fd
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-08 18:07:55 +0330

    edit code

--- app/routers/api/v1/profile.js
+++ app/routers/api/v1/profile.js
@@ -3,17 +3,17 @@ const router = express.Router();
 
 
 
-//>---------- controller 
+//*>---------- controller 
 const userController = require("app/http/controllers/usersControllers/usersController");
 
-//>---------- middleware 
+//*>---------- middleware 
 const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
 const verifyRoles = require("app/http/middleware/authMiddleware/verifyRoles");
 
 const ROLES_LIST = require("app/config/roles_list");
 
 
-//>---------- post route 
+//*>---------- post route 
 
 router.get("/", verifyJWT, userController.getSingleUser);
 
