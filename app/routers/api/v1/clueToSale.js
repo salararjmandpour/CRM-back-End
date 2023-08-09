0c90cdc1e61db39d8040964a34f2c2db152e3c41
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-09 15:01:15 +0330

    create route sale

---
+++ app/routers/api/v1/clueToSale.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const ROLES_LIST = require("../../../config/roles_list");
+
+//*>---------- controller
+
+const clueToSaleController = require("app/http/controllers/modulesControllers/clueToSaleController");
+
+//*>---------- middleware
+
+const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
+const verifyRoles = require("app/http/middleware/authMiddleware/verifyRoles");
+
+//*>----------- create route for clue
+
+// router.get("/singleandall", verifyJWT, clueController.getOneAndAllHandler);
+
+router.post(
+  "/",
+  verifyJWT,
+  verifyRoles(
+    ROLES_LIST.SeniorManager
+    // ROLES_LIST.SalesSpecialist,
+    // ROLES_LIST.SalesManager,
+    // ROLES_LIST.FinanceManager,
+    // ROLES_LIST.AccountingManager,
+    // ROLES_LIST.Accountant,
+    // ROLES_LIST.CRMManager
+  ),
+  clueToSaleController.createHandlerNew
+);
+
+module.exports = router;
