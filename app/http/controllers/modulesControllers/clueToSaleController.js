e47772a5b030a17a93473330031bf4a77061226c
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-09 16:14:24 +0330

    edit code

--- app/http/controllers/modulesControllers/clueToSaleController.js
+++ app/http/controllers/modulesControllers/clueToSaleController.js
@@ -1,6 +1,4 @@
-const User = require("app/models/User");
 const Clues = require("app/models/Clue");
-const Contact = require("app/models/Contact");
 const Sale = require("app/models/Sale");
 
 //*>---------- encrypt data sending
@@ -64,28 +62,54 @@ const createHandlerNew = async (req, res) => {
   }
 };
 
-//*>---------- get method all sale
+//*>---------- get method all sale by user
 
 const getByUserHandler = async (req, res) => {
-  const strId = req.query.id.toString();
-  const strIdNew = strId.replaceAll(" ", "+");
-  const decryptUserId = cerateCipher.decrypt(strIdNew, Key);
-  try {
-    //!>----------- get all  model for data user
-    const saleAll = await Sale.find({ "expert.expertId": decryptUserId });
-    console.log(saleAll);
+  if (req.query.id) {
+    const strId = req.query.id.toString();
+    const strIdNew = strId.replaceAll(" ", "+");
+    const decryptUserId = cerateCipher.decrypt(strIdNew, Key);
+    try {
+      //!>----------- get all  model for data user
+      const saleAll = await Sale.find({ "expert.expertId": decryptUserId });
 
-    if (!saleAll || saleAll.length == 0) {
-      return res.status(404).json({
-        status: 404,
-        message: "فروشی ثبت نشده است",
-      });
+
+      if (!saleAll || saleAll.length == 0) {
+        return res.status(404).json({
+          status: 404,
+          message: "فروشی ثبت نشده است",
+        });
+      }
+      const encryptData = cerateCipher.encrypt(JSON.stringify(saleAll), Key);
+      return res.status(202).json({ encryptData });
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).json({ message: err.message });
     }
-    const encryptData = cerateCipher.encrypt(JSON.stringify(saleAll), Key);
-    return res.status(202).json({ encryptData });
-  } catch (err) {
-    console.log(err.message);
-    return res.status(500).json({ message: err.message });
+  }
+
+  //*>---------- get method  single sale by id sale
+
+  if (req.query.saleId) {
+    const strId = req.query.saleId.toString();
+    const strIdNew = strId.replaceAll(" ", "+");
+    const decryptSaleId = cerateCipher.decrypt(strIdNew, Key);
+
+    try {
+      const sale = await Sale.findOne({ _id: decryptSaleId });
+console.log(sale);
+      if (!sale || sale.length == 0) {
+        return res.status(404).json({
+          status: 404,
+          message: "سرنخی ثبت نشده است",
+        });
+      }
+      const encryptData = cerateCipher.encrypt(JSON.stringify(sale), Key);
+      return res.status(202).json({ encryptData });
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).json({ message: err.message });
+    }
   }
 };
 
