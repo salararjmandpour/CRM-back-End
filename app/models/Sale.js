0194c11b8bc672a16ca2686ff9883937c93b4a67
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-09 15:00:47 +0330

    create model sale

---
+++ app/models/Sale.js
@@ -0,0 +1,118 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
+const Schema = mongoose.Schema;
+
+const saleSchema = new Schema(
+  {
+    subject: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 100,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 100,
+    },
+    role: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 100,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 10,
+      maxLength: 12,
+    },
+    industry: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 25,
+    },
+    expert: {
+      expertId: { type: String, required: true, trim: true },
+      expertFullName: { type: String, required: true, trim: true },
+    },
+    company: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 100,
+      default: null,
+    },
+    step: {
+      evaluation: { type: String, default: "15%" },
+      PreliminaryNegotiations: { type: String },
+      invoice: { type: String },
+      finalNegotiations: { type: String },
+      closed: { type: String },
+    },
+    totalAmount: {
+      type: Number,
+      default:0,
+      unique: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 9,
+      maxLength: 12,
+    },
+    address: {
+      address: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 2,
+        maxLength: 300,
+      },
+      state: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 2,
+        maxLength: 50,
+      },
+      cities: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 2,
+        maxLength: 50,
+      },
+    },
+    qualityCustomer: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    dateForSale: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 100,
+    },
+    campaign: [String],
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+// clueSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("sale", saleSchema);
