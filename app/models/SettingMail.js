2e80c95d2acf1a514f8091e57e4ecc614c36edfa
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-16 09:52:44 +0330

    edit code

--- app/models/SettingMail.js
+++ app/models/SettingMail.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const settingMail = new Schema({
-  host: {
-    type: String,
-    required: true,
+const settingMail = new Schema(
+  {
+    host: {
+      type: String,
+      required: true,
+    },
+    port: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    user: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    pass: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-  port: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  user: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  pass: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-});
+  { timestamps: true, toJSON: { virtuals: true } }
+);
 
 module.exports = mongoose.model("SettingMail", settingMail);
