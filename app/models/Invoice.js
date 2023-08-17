aec594ffdecea0e8a11e16104222d9c8c932d6cb
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-17 13:24:17 +0330

    create invoice model

---
+++ app/models/Invoice.js
@@ -0,0 +1,107 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
+const Schema = mongoose.Schema;
+
+const invoiceSchema = new Schema(
+  {
+    numberOfInvoice: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    nameOfInvoice: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 100,
+    },
+    statusInvoice: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 100,
+    },
+    nameOfAgent: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 100,
+    },
+    economicCode: {
+      type: Number,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minlength: 2,
+      maxLength: 100,
+    },
+    wayOfPost: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    wayOfPay: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    addressOfProduct: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    addressOfInvoice: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    sumTax: {
+      type: Number,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    finalPrice: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    expert: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    sale: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+// clueSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("invoice", invoiceSchema);
