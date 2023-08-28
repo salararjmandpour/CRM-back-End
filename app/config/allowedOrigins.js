794f6a45c0a81eea105d1c0c35518afd8c093187
SalarArjmandpour <102907879+salararjmandpour@users.noreply.github.com>
2023-08-28 15:18:53 +0330

    import cors

--- app/config/allowedOrigins.js
+++ app/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://www.mysite.com",
   "http://localhost:3000",
+  "https://jsondoc.iran.liara.run",
 ];
 
 module.exports = allowedOrigins;
