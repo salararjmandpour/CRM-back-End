6a63de14aef9dc7ef8ad5c63bd9ebd8c117201f7
salararjmandpour <salararjmand4@gmail.com>
2023-10-19 13:08:30 +0330

    create model ActivitySaleTellOpen for sale

---
+++ app/models/ActivitySaleTellOpen.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ActivitySaleTellOpenSchema = new Schema(
+  {
+    activityTellSubject: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxLength: 100,
+    },
+    activityTellNote: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    activityTellTime: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    stepTell: {
+      isActive: { type: Boolean, default: false },
+      evaluation: { type: Boolean },
+      PreliminaryNegotiations: { type: Boolean },
+    },
+    activityTellDate: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    cancelationReason: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    saleId: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    saleName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+module.exports = mongoose.model(
+  "ActivitySaleTellOpen",
+  ActivitySaleTellOpenSchema
+);
