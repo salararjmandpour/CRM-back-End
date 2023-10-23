81e26f6ba5a09c5203634681d02a710a97250f93
salararjmandpour <salararjmand4@gmail.com>
2023-10-23 18:54:08 +0330

    import sale meet and tell for notifications

--- app/http/controllers/modulesControllers/notificationsControllers.js
+++ app/http/controllers/modulesControllers/notificationsControllers.js
@@ -3,6 +3,8 @@ const Clues = require("app/models/Clue");
 const ActivityCluesMeetOpen = require("app/models/ActivityCluesMeetOpen");
 const ActivityCluesTellOpen = require("app/models/ActivityCluesTellOpen");
 const DutiesSale = require("app/models/DutiesSale");
+const ActivitySaleMeetOpen = require("app/models/ActivitySaleMeetOpen");
+const ActivitySaleTellOpen = require("app/models/ActivitySaleTellOpen");
 
 //*>---------- encrypt data sending
 
@@ -27,12 +29,49 @@ const getNotifications = async (req, res) => {
       },
       {
         _id: 0,
-        subjectForDuties: 0,
         explainForDuties: 0,
         dateForDuties: 0,
         timeForDuties: 0,
         createdAt: 0,
         isActive: 0,
+        updatedAt: 0,
+        userId: 0,
+        __v: 0,
+      }
+    );
+
+    let findSaleMeet = await ActivitySaleMeetOpen.find(
+      {
+        userId: decryptUserId,
+        "status.isActive": false,
+      },
+      {
+        _id: 0,
+        activityNote: 0,
+        activityLocation: 0,
+        cancelationReason: 0,
+        activityParticipatingPeople: 0,
+        activityPriority: 0,
+        createdAt: 0,
+        isActive: 0,
+        updatedAt: 0,
+        userId: 0,
+      }
+    );
+
+    let findSakeTell = await ActivitySaleTellOpen.find(
+      {
+        userId: decryptUserId,
+        "status.isActive": false,
+      },
+      {
+        _id: 0,
+        activityTellNote: 0,
+        createdAt: 0,
+        isActive: 0,
+        cancelationReason: 0,
+        activityCallType: 0,
+        activityCallPriority: 0,
         updatedAt: 0,
         userId: 0,
         __v: 0,
@@ -86,10 +125,24 @@ const getNotifications = async (req, res) => {
       JSON.stringify(findDutiesSale),
       Key
     );
+    const encryptDataSaleMeet = cerateCipher.encrypt(
+      JSON.stringify(findSaleMeet),
+      Key
+    );
+    const encryptDataSaleTell = cerateCipher.encrypt(
+      JSON.stringify(findSakeTell),
+      Key
+    );
 
     return res
       .status(200)
-      .json({ encryptDataMeet, encryptDataTell, encryptDataDutiesSale });
+      .json({
+        encryptDataMeet,
+        encryptDataTell,
+        encryptDataDutiesSale,
+        encryptDataSaleMeet,
+        encryptDataSaleTell,
+      });
   } catch (err) {
     console.log(err.message);
     return res.status(500).json({ message: err });
