e1b983d8ece8b6f8428314b909d758d3c6cd6a81
salararjmandpour <salararjmand4@gmail.com>
2023-10-30 11:38:48 +0330

    create method edit password user by admin

--- app/routers/api/v1/register.js
+++ app/routers/api/v1/register.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+//*>---------- middleware
+
+const verifyJWT = require("app/http/middleware/authMiddleware/verifyJWT");
+const verifyRoles = require("app/http/middleware/authMiddleware/verifyRoles");
+
 //*>----------- import controllers
 
 const registerController = require("app/http/controllers/authControllers/registerController");
 
+//*>----------- create route for User
+
 router.post("/", registerController.register);
+
+router.post(
+  "/edit",
+  verifyJWT,
+  verifyRoles(ROLES_LIST.SeniorManager),
+  registerController.editUserPasswordByAdmin
+);
+
 
 module.exports = router;
