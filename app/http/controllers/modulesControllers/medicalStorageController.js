86320de6810d7078a14ce0a311f524fb0de73928
salararjmandpour <salararjmand4@gmail.com>
2023-11-04 18:13:02 +0330

    edit code

--- app/http/controllers/modulesControllers/medicalStorageController.js
+++ app/http/controllers/modulesControllers/medicalStorageController.js
@@ -11,17 +11,15 @@ const Key = config.encryptionKey;
 //*>----------- create function search in medical storage
 
 const searchMedicalStorage = async (req, res) => {
+  //*>--------- search by name product
   if (req.query.searchName) {
     const strSearch = req.query.searchName.toString();
     const strSearchNew = strSearch.replaceAll(" ", "+");
     const decryptSearch = await cerateCipher.decrypt(strSearchNew, Key);
 
-    console.log(decryptSearch);
-
     try {
       const products = await MedicalStorage.find({
         nameProduct: { $regex: decryptSearch, $options: "i" }, // Case-insensitive search
-        // sku: { $regex: decryptSearch, $options: "i" },
       }).limit(20);
 
       const encryptProducts = cerateCipher.encrypt(
@@ -33,17 +31,20 @@ const searchMedicalStorage = async (req, res) => {
       console.error(error);
       res.status(500).json({ error: "Server error" });
     }
-  } else {
+  }
+  //*>----------- search by SKU product
+  else {
     const strSku = req.query.searchSku.toString();
     const strSkuNew = strSku.replaceAll(" ", "+");
     const decryptSearchSku = await cerateCipher.decrypt(strSkuNew, Key);
 
-    console.log(decryptSearchSku);
     try {
       const sku = await MedicalStorage.find({
         sku: { $regex: decryptSearchSku, $options: "i" },
       }).limit(20);
+
       const encryptProducts = cerateCipher.encrypt(JSON.stringify(sku), Key);
+
       return res.status(202).json({ encryptProducts });
     } catch (error) {
       console.error(error);
