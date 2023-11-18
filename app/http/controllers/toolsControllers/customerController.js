b53eb71e8724e770ca9d91585bbb8dc0a485086b
salararjmandpour <salararjmand4@gmail.com>
2023-11-18 11:18:33 +0330

    create controller by customer

---
+++ app/http/controllers/toolsControllers/customerController.js
@@ -0,0 +1,38 @@
+const Customer = require("app/models/Customer");
+
+//*>---------- encrypt data sending
+
+const cerateCipher = require("../../middleware/cerateCipher");
+const Key = config.encryptionKey;
+
+//*>---------- method of create customer
+
+const createCustomer = async (req, res) => {
+  const data = await req.body.data;
+
+  if (!data.firstName && !data.lastName && !data.mobile) {
+    return res.status(400);
+  }
+
+  const { firstName, lastName, mobile, comment, job, subject } = data;
+
+  try {
+
+    await Customer.create({
+      firstName,
+      lastName,
+      mobile,
+      comment,
+      job,
+      subject,
+    });
+
+    res.sendStatus(201);
+    
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ message: err });
+  }
+};
+
+module.exports = { createCustomer };
